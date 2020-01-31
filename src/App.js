ec3e0406611e1e87f87af533325f02e3f3a018f0
bagusadiw <bagusadi1494@gmail.com>
2020-01-31 20:57:34 +0700

    basename

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component{
   render(){
     return (
       <div>
-        <Router>
+        <Router basename="/">
           <Header />
           <Switch>
             <Route exact path="/" component={Home} />
