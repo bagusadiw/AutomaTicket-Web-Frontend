d4bf4f77e8da694e8fd52a045c9d63b0a36870af
bagusadiw <bagusadi1494@gmail.com>
2020-01-31 20:50:16 +0700

    Updates

---
+++ precache-manifest.f01ad7a17860d953d995af7160e508c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ea16be475ef06a2ec1bb9bba5c0136",
+    "url": "/AutomaTicket-Web-Frontend/index.html"
+  },
+  {
+    "revision": "851f899963e3898a83c2",
+    "url": "/AutomaTicket-Web-Frontend/static/css/main.9e0c16e3.chunk.css"
+  },
+  {
+    "revision": "9d8140af16d028c6ce1e",
+    "url": "/AutomaTicket-Web-Frontend/static/js/2.5f8489ee.chunk.js"
+  },
+  {
+    "revision": "26c31c05639d8c8e4d046eb1b98016c3",
+    "url": "/AutomaTicket-Web-Frontend/static/js/2.5f8489ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "851f899963e3898a83c2",
+    "url": "/AutomaTicket-Web-Frontend/static/js/main.6b41f0bc.chunk.js"
+  },
+  {
+    "revision": "785b4811d3273e3bc7ca",
+    "url": "/AutomaTicket-Web-Frontend/static/js/runtime-main.0f50820b.js"
+  }
+]);
